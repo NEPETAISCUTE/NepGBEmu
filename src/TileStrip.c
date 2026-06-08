929882121cd313230a2a298bfb0de684dd2e6b03
NEPETAISCUTE <nepecute@gmail.com>
2026-06-08 11:37:02 +0200

    fixed bug where it would fetch tiles too far if tileID is 0x80 or beyond

--- src/TileStrip.c
+++ src/TileStrip.c
@@ -9,8 +9,8 @@ TileStrip* TileStripCreate(PPU* ppu, u8 tileID, u8 scanline, u16 blockOrigin) {
 
 	if (tileID >= 128) blockOrigin = 0x8800;
 	u8 relY = scanline % 8;
-	u8 byteHigh = MemoryBusRead(ppu->bus, blockOrigin + tileID * 0x10 + relY * 2, false);
-	u8 byteLow = MemoryBusRead(ppu->bus, blockOrigin + tileID * 0x10 + relY * 2 + 1, false);
+	u8 byteHigh = MemoryBusRead(ppu->bus, blockOrigin + (tileID % 128) * 0x10 + relY * 2, false);
+	u8 byteLow = MemoryBusRead(ppu->bus, blockOrigin + (tileID % 128) * 0x10 + relY * 2 + 1, false);
 	for (u8 i = 0; i < 8; i++) {
 		u8 colorIndex = BuildU2(GetBit(byteHigh, i), GetBit(byteLow, i));
 		// printf("tileID = %d, relY = %d, relX = %d, colorIndex = %d\n", tileID, relY, 7 - i, colorIndex);
