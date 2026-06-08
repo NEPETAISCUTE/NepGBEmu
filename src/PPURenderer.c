4e01212dbaaaf5785ac7efb51de0d5d0f63a29b4
NEPETAISCUTE <nepecute@gmail.com>
2026-06-08 11:36:41 +0200

    added function to render tileset

--- src/PPURenderer.c
+++ src/PPURenderer.c
@@ -18,6 +18,7 @@ u32 PPURendererDrawScanline(PPU* ppu) {
 		u32 tileIndex = tileVert * 0x20 + tileHorz;
 
 		u8 tileId = MemoryBusRead(ppu->bus, VRAM_TILE_MAP + tileIndex, false);
+		// if (ppu->frame == 0 && tileVert == 2 && y % 8 == 0) printf("tile %X at index %d,%d\n", tileId, tileHorz, tileVert);
 
 		// TODO: add support for LCDC.4 = 1
 		TileStrip* strip = TileStripCreate(ppu, tileId, y, 0x8000);
@@ -35,4 +36,31 @@ u32 PPURendererDrawScanline(PPU* ppu) {
 	}
 
 	return 1;  // temporary
+}
+
+void PPURendererDrawTilesetScanline(PPU* ppu) {
+	const u32 y = ppu->scanline;
+
+	const u32 tileVert = y / 8;
+
+	for (u32 tileHorz = 0; tileHorz < 0x10 /*(PPU_SCREEN_WIDTH / 8)*/; tileHorz++) {
+		u32 tileIndex = tileVert * 0x10 + tileHorz;
+
+		u8 tileId = MemoryBusRead(ppu->bus, VRAM_TILE_MAP + tileIndex, false);
+		if (ppu->frame == 0 && tileVert == 3 && y % 8 == 0) printf("tile %d at index %d,%d\n", tileId, tileHorz, tileVert);
+
+		// TODO: add support for LCDC.4 = 1
+		TileStrip* strip = TileStripCreate(ppu, tileIndex, y, 0x8000);
+
+		for (u8 fineX = 0; fineX < 8; fineX++) {
+			u32 x = tileHorz * 8 + fineX;
+
+			u8 colorIndex = TileStripGetColorIndex(strip, fineX);
+			Color c = DMG_MASTER_PALETTE[colorIndex % 4];
+			// printf("displaying pixel %X%X%X%X at x = %d, y = %d\n", c.a, c.b, c.g, c.r, x, y);
+
+			PPUPlot(ppu, x, y, c);
+		}
+		TileStripDestroy(strip);
+	}
 }
