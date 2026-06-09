b15c684e6547e1ffeaf3e726df2c2790e6b2b908
NEPETAISCUTE <nepecute@gmail.com>
2026-06-10 01:00:20 +0200

    added addresses for ppu registers (simplifies handling read writes)

--- include/PPURegisters.h
+++ include/PPURegisters.h
@@ -3,6 +3,21 @@
 
 #include "common.h"
 
+typedef enum PPURegisterAddress {
+	PPUREGS_ADDRESS_LCDC = 0xFF40,
+	PPUREGS_ADDRESS_STAT = 0xFF41,
+	PPUREGS_ADDRESS_SCY = 0xFF42,
+	PPUREGS_ADDRESS_SCX = 0xFF43,
+	PPUREGS_ADDRESS_LY = 0xFF44,
+	PPUREGS_ADDRESS_LYC = 0xFF45,
+	PPUREGS_ADDRESS_DMA = 0xFF46,
+	PPUREGS_ADDRESS_BGP = 0xFF47,
+	PPUREGS_ADDRESS_OBP0 = 0xFF48,
+	PPUREGS_ADDRESS_OBP1 = 0xFF49,
+	PPUREGS_ADDRESS_WY = 0xFF4A,
+	PPUREGS_ADDRESS_WX = 0xFF4B,
+} PPURegisterAddress;
+
 typedef struct PPURegisters {
 	// joypad
 
@@ -32,5 +47,8 @@ typedef struct PPURegisters {
 	// CGB only
 
 } PPURegisters;
+
+u8 PPURegistersRead(PPURegisters* regs, u16 address);
+void PPURegistersWrite(PPURegisters* regs, u16 address, u8 value);
 
 #endif	// NEPGB_PPU_REGISTERS_H
