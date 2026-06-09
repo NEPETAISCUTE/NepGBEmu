0b8726bf8de9840ae13548f3921f071820982107
NEPETAISCUTE <nepecute@gmail.com>
2026-06-10 01:08:22 +0200

    reordered the palette so that it's correct

--- src/MasterPalette.c
+++ src/MasterPalette.c
@@ -1,17 +1,11 @@
 #include "MasterPalette.h"
 
 const Color DMG_MASTER_PALETTE[] = {
+
 	(Color){
 		.r = 0x9a,
 		.g = 0x9e,
 		.b = 0x3f,
-		.a = 0xFF,
-	},
-
-	(Color){
-		.r = 0x0e,
-		.g = 0x45,
-		.b = 0x0b,
 		.a = 0xFF,
 	},
 
@@ -23,9 +17,25 @@ const Color DMG_MASTER_PALETTE[] = {
 	},
 
 	(Color){
+		.r = 0x0e,
+		.g = 0x45,
+		.b = 0x0b,
+		.a = 0xFF,
+	},
+
+	(Color){
 		.r = 0x1b,
 		.g = 0x2a,
 		.b = 0x09,
+		.a = 0xFF,
+	},
+
+	// whiter than color 0
+
+	(Color){
+		.r = 0xda,
+		.g = 0xde,
+		.b = 0x8e,
 		.a = 0xFF,
 	},
 
